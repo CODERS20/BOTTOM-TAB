ea44f6f90963cb1d12140da889a823feea8e1c20
CODERS20 <ajeetpinku@gmail.com>
2021-04-25 23:12:47 +0530

    BOTTOM TAB NAVIGATION

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import FacebookScreen from './screens/fb';
+import InstagramScreen from './screens/in'
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text style={{fontWeight:'bold',fontSize:19,color:'#92e4e6'}}></Text>
+        <AppContainer></AppContainer>
+      </View>
+      
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const TabNavigator = createBottomTabNavigator({
+    Facebook: {screen:FacebookScreen},
+    Instagram: {screen:InstagramScreen}
+})
+
+const AppContainer = createAppContainer(TabNavigator)
